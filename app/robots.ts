f3968a21b7932efda5261952e00cd2750206e46c
Şükrü Gençoğlu <sukrugencoglu10@gmail.com>
2026-02-16 12:20:21 +0300

    fix(seo): correct robots.ts rules format to array

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/'],
+      },
+    ],
     sitemap: 'https://pekcon.com.tr/sitemap.xml',
   };
 }
